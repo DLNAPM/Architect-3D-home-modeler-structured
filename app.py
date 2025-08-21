9fc58348d5f7a1432ed8381b21415fa18f40876e
DLNAPM <DLaniger.Napm.Consulting@gmail.com>
2025-08-21 08:19:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -442,4 +442,4 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)```
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
